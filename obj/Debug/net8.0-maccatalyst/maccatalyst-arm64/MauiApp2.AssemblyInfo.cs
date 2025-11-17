2a47724faa81166c67d374e2b8220b64a1d488a1
svnelif <elifatsvn2003@gmail.com>
2025-11-17 14:06:51 +0300

    Gerçek proje klasörü yüklendi

--- obj/Debug/net8.0-maccatalyst/maccatalyst-arm64/MauiApp2.AssemblyInfo.cs
+++ obj/Debug/net8.0-maccatalyst/maccatalyst-arm64/MauiApp2.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MauiApp2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+f2f8bb428f91c763683959028cd6a5b62b51692a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+5bbbe88d99d94ca3454d77912440fc17b5dee9c8")]
 [assembly: System.Reflection.AssemblyProductAttribute("MauiApp2")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MauiApp2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
